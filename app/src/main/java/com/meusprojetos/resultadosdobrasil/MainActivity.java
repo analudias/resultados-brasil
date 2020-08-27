7db8edb4fcf4bfe796f622ba8008f8ae40f37146
Ana Luisa Dias <analudias@live.com>
2020-08-27 13:59:59 -0300

    Adicionando a activity para seleção principal de futebol

--- app/src/main/java/com/meusprojetos/resultadosdobrasil/MainActivity.java
+++ app/src/main/java/com/meusprojetos/resultadosdobrasil/MainActivity.java
@@ -3,12 +3,27 @@ package com.meusprojetos.resultadosdobrasil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageFutebol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageFutebol = findViewById(R.id.imageFutebol);
+
+        imageFutebol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
     }
 }
