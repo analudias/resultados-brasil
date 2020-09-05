d4b1552ec8a0774c73b7572b05edd616a1ca3a4b
Ana Luisa Dias <analudias@live.com>
2020-09-05 19:05:11 -0300

    ajustes na tela de adapter do esporte

--- app/src/main/java/com/meusprojetos/resultadosdobrasil/adapter/EsportesAdapter.java
+++ app/src/main/java/com/meusprojetos/resultadosdobrasil/adapter/EsportesAdapter.java
@@ -2,6 +2,8 @@ package com.meusprojetos.resultadosdobrasil.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,7 +14,26 @@ import java.util.List;
 
 public class EsportesAdapter extends RecyclerView.Adapter<EsportesAdapter.MyViewHolder> {
 
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView imagemEsporte;
+        private TextView textEsporte;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
